cda9cb5e8a8208d08559da2c6234b886bdf95266
jameer <jimcute8879@gmail.com>
2015-12-18 02:06:19 +0530

    contact us page added

--- app/controllers/helpController.js
+++ app/controllers/helpController.js
@@ -4,7 +4,10 @@
 })();
 
 
-app.controller('contactUsController', function ($scope, $window) {
+app.controller('contactUsController', function ($scope, $window, ENV) {
+    $scope.message = "hello contact us page";
+    $scope.companyName = ENV.appName;
+
 });
 
 app.controller('faqController', function ($scope, $window) {
